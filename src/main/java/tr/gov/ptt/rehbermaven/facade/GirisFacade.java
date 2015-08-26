a2c88929cc091ede01ac894aed71749c87396019
Administrator <Administrator@10.0.0.184>
2015-08-26 12:37:29 +0300

    Giris bölümü eklendi facade try catch eklendi

--- src/main/java/tr/gov/ptt/rehbermaven/facade/GirisFacade.java
+++ src/main/java/tr/gov/ptt/rehbermaven/facade/GirisFacade.java
@@ -2,6 +2,7 @@ package tr.gov.ptt.rehbermaven.facade;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import tr.gov.ptt.rehbermaven.entity.Giris;
 
@@ -21,11 +22,15 @@ public class GirisFacade extends AbstractFacade<Giris> {
     }
     public boolean girisKontrol(Giris p_giris)
     {
+        try{
+            
+        
         Giris giris=em.createNamedQuery("Giris.girisKontrol",Giris.class).setParameter("kullanici",p_giris.getKullanici()).setParameter("sifre",p_giris.getSifre())
                 .getSingleResult();
-        if (giris !=null) {
-            return true;
-        } else {
+       return true;
+        }
+        catch(NoResultException nre)
+        {
             return false;
         }
         
